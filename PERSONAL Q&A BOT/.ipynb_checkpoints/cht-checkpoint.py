8c32aa3aa4749c3e28142ab267324c4bc0bcc729
Sujith <sujithcholleti315@gmail.com>
2025-01-09 15:03:47 -0500

    Modified the main_file

---
+++ PERSONAL Q&A BOT/.ipynb_checkpoints/cht-checkpoint.py
@@ -0,0 +1,73 @@
+
+
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain 
+from langchain_community.chat_models import ChatOpenAI
+
+OPENAI_API_KEY="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"
+
+
+#pdf upload section
+st.header("My First Personal Chat bot")
+with st.sidebar:
+    st.title("Your Documents")
+    file=st.file_uploader("Please Upload your PDF File and start asking questions",type="pdf")
+
+#text Extraction
+if file is not None:
+    pdf_reader=PdfReader(file)
+    text=""
+    for page in pdf_reader.pages:
+        text=text+page.extract_text()
+        #st.write(text)
+#split into chunks
+    text_splitter=RecursiveCharacterTextSplitter(
+    separators="\n",
+    chunk_size=1000,
+    chunk_overlap=150,
+    length_function=len
+    )
+
+    chunks= text_splitter.split_text(text)
+    #st.write(chunks)
+
+#generating embeddings
+    embeddings=OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    #embeddings = GeminiEmbeddings(api_key=GEMINI_API_KEY)
+
+    
+    #creating vector store - FAISS
+    
+    vector_store=FAISS.from_texts(chunks,embeddings)
+
+    #user Question 
+    user_question=st.text_input("Type your Questions here")
+
+    #similarity search
+    if user_question:
+        match=vector_store.similarity_search(user_question)
+        #st.write(match)
+
+        #defining LLM
+        # llm = GeminiChatModel(
+        # api_key=GEMINI_API_KEY,
+        # temperature=0,
+        # max_tokens=1000,
+        # model_name="Gemini 1.5 Flash-8B"
+        # )
+
+        llm=ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            temperature=0,
+            max_tokens=1000,
+            model_name="gpt-3.5-turbo"
+        )
+        #output results
+        chain=load_qa_chain(llm,chain_type="stuff")
+        res=chain.run(input_documents=match,question=user_question)
+        st.write(res)
+
