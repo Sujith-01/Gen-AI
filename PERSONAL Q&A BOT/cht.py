0999c584a44aac20ddd3f1824a88b6aab9b34fa4
Sujith <sujithcholleti315@gmail.com>
2025-01-09 22:00:01 -0500

    just made a modification

--- PERSONAL Q&A BOT/cht.py
+++ PERSONAL Q&A BOT/cht.py
@@ -8,7 +8,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain 
 from langchain_community.chat_models import ChatOpenAI
 
-#OPENAI_API_KEY="replace with open ai api key here"
+OPENAI_API_KEY="replace with open ai api key here"
 
 
 #pdf upload section
